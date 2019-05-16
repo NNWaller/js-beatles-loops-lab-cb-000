0c51b66707d55b5540fcc04c0a3bfaa038111f35
Natasha Waller <nnwaller@gmail.com>
2019-05-16 15:26:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead guitar", "Drums"];
+var jamArray = [];
+function theBeatlesPlay (musicians, instruments){
+  for (let i= 0, j= 0; i< musicians.length; i++, j++){
+    jamArray.push (`${musicians[i]} plays ${instruments[j]}`);
+  } return jamArray;
+}
+
+var i = 0
+function johnLennonFacts (facts){
+  while (i < facts.length){
+    (facts[i]).concat("!!!");
+    i++;
+  } 
+  return facts;
+}
